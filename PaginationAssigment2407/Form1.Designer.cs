928572e59dc78a15670da0c9bb4e4507a4084cb4
Zakawat <31027252+zakawat@users.noreply.github.com>
2018-11-12 19:31:21 +0500

    Added all files

---
+++ PaginationAssigment2407/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PaginationAssigment2407
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtNoOfRecords = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRecodsDisplay = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(652, 474);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(12, 512);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "<< Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(542, 509);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(122, 26);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next >>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtNoOfRecords
+            // 
+            this.txtNoOfRecords.Location = new System.Drawing.Point(168, 6);
+            this.txtNoOfRecords.Name = "txtNoOfRecords";
+            this.txtNoOfRecords.Size = new System.Drawing.Size(117, 20);
+            this.txtNoOfRecords.TabIndex = 3;
+            this.txtNoOfRecords.Text = "20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "No of Pages/Recods to Show";
+            // 
+            // lblRecodsDisplay
+            // 
+            this.lblRecodsDisplay.AutoSize = true;
+            this.lblRecodsDisplay.Location = new System.Drawing.Point(177, 517);
+            this.lblRecodsDisplay.Name = "lblRecodsDisplay";
+            this.lblRecodsDisplay.Size = new System.Drawing.Size(35, 13);
+            this.lblRecodsDisplay.TabIndex = 5;
+            this.lblRecodsDisplay.Text = "label2";
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(291, 6);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(95, 20);
+            this.btnReload.TabIndex = 6;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 539);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.lblRecodsDisplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNoOfRecords);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtNoOfRecords;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRecodsDisplay;
+        private System.Windows.Forms.Button btnReload;
+    }
+}
+
---
+++ PaginationAssigment2407/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaginationAssigment2407
+{
+    public partial class Form1 : Form
+    {
+        private String conString = @"Data Source=ZAKIPC;Initial Catalog=systemPrograming;User ID=sa;Password=XXXXXX";
+
+        private int numberOfRecordsToDisplay = 0, displayedRecordStartPosition = 0, displayedRecordEndPosition = 0;
+
+        private DataTable dt = new DataTable();
+        private int totalNumberOfRecordsInDataset;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Students;", new SqlConnection(conString));
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            
+            sda.Fill(dt);
+
+            foreach (DataRow item in dt.Rows)
+            {
+                System.Diagnostics.Debug.WriteLine($"{item[0]}, {item[1]}, {item[2]}, {item[3]}");
+            }
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+
+
+            totalNumberOfRecordsInDataset = dt.AsEnumerable().Count();
+            btnReloadFunctionality();
+            
+
+
+
+        }
+
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            btnReloadFunctionality();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            displayedRecordEndPosition = displayedRecordStartPosition;
+            displayedRecordStartPosition -= numberOfRecordsToDisplay;
+            loadDataIntoGridView();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            displayedRecordStartPosition = displayedRecordEndPosition;
+            displayedRecordEndPosition += numberOfRecordsToDisplay;
+            loadDataIntoGridView();
+        }
+
+        private void btnReloadFunctionality()
+        {
+            try
+            {
+                displayedRecordStartPosition = 0;
+                numberOfRecordsToDisplay = int.Parse(txtNoOfRecords.Text);
+                displayedRecordEndPosition = int.Parse(txtNoOfRecords.Text);
+
+                if (numberOfRecordsToDisplay <= 0)
+                {
+                    MessageBox.Show("The record can not be Zero or Negative Number !\n\nSetting back to default value. i.e 20");
+                    numberOfRecordsToDisplay = displayedRecordEndPosition = 20;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show("Setting back to default value. i.e 20");
+                numberOfRecordsToDisplay = 20;
+                txtNoOfRecords.Text = "20";
+            }
+            loadDataIntoGridView();
+        }
+
+
+
+        private void loadDataIntoGridView()
+        {
+
+            if (displayedRecordStartPosition == 0)
+            {
+                //first Page !
+                btnPrevious.Enabled = false;
+                btnNext.Enabled = true;
+                //dataGridView1.DataSource = dt.AsEnumerable().Take(numberOfRecordsToDisplay).CopyToDataTable();
+                dataGridView1.DataSource = dt.AsEnumerable().Skip(displayedRecordStartPosition).Take(numberOfRecordsToDisplay).CopyToDataTable();
+
+
+            }
+            else if (displayedRecordEndPosition == totalNumberOfRecordsInDataset)
+            {
+                //Last Page !
+
+                btnPrevious.Enabled = true;
+                btnNext.Enabled = false;
+                dataGridView1.DataSource = dt.AsEnumerable().Skip(displayedRecordStartPosition).Take(numberOfRecordsToDisplay).CopyToDataTable();
+                
+            }
+            else
+            {
+                //Middile Pages
+
+                btnPrevious.Enabled = true;
+                btnNext.Enabled = true;
+                dataGridView1.DataSource = dt.AsEnumerable().Skip(displayedRecordStartPosition).Take(numberOfRecordsToDisplay).CopyToDataTable();
+
+            }
+
+                updateNavigationalInfoLabel();
+
+
+        }
+
+        private void updateNavigationalInfoLabel()
+        {
+            lblRecodsDisplay.Text = $"Showing {displayedRecordStartPosition+1} to {displayedRecordEndPosition} records on screen out of {totalNumberOfRecordsInDataset}";
+        }
+    }
+}
